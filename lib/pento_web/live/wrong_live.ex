a18489a5c0eeda2d7d12686685c110fff806146c
Cornelia Kelinske <corneliakelinske@gmail.com>
2022-05-17 14:01:23 -0700

    make game work

--- lib/pento_web/live/wrong_live.ex
+++ lib/pento_web/live/wrong_live.ex
@@ -1,8 +1,12 @@
 defmodule PentoWeb.WrongLive do
   use Phoenix.LiveView, layout: {PentoWeb.LayoutView, "live.html"}
 
+  alias PentoWeb.Router.Helpers, as: Routes
+
+
+
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, score: 0, message: "Make a guess:", time: time())}
+    {:ok, assign(socket, score: 10, message: "Make a guess:", lucky_number: lucky_number(), time: time(), game_won?: false)}
   end
 
   def render(assigns) do
@@ -17,15 +21,33 @@ defmodule PentoWeb.WrongLive do
     <a href="#" phx-click="guess" phx-value-number= {n} ><%= n %></a>
     <% end %>
     </h2>
+    <%= if @game_won? == true do %>
+    <btn> <%= live_patch "Play again!", to: Routes.live_path(@socket, PentoWeb.WrongLive) %> </btn>
+
+    <% end %>
 
     """
   end
 
 
 
-  def handle_event("guess", %{"number" => guess}=data, socket) do
+  def handle_event("guess", %{"number" => guess}=data, %{assigns: %{lucky_number: lucky_number}} = socket) do
+    case guess do
+     ^lucky_number -> message = "You win"
+      {
+        :noreply,
+        assign(
+          socket,
+          message: message,
+          time: time(),
+          game_won?: true
+        )
+      }
+    _ ->
     message = "Your guess: #{guess}. Wrong. Guess again. "
     score = socket.assigns.score - 1
+
+
 
     {
       :noreply,
@@ -35,10 +57,17 @@ defmodule PentoWeb.WrongLive do
         score: score,
         time: time()
       )
-    }
+      }
+    end
   end
 
-  
+  def handle_params(_params, _url, socket) do
+    {:noreply, socket}
+    end
+
+  def lucky_number() do
+    Enum.random(1..10) |> to_string
+  end
 
   def time() do
     DateTime.utc_now |> to_string
