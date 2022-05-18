c4ea8e857a686bcd465cfaa6a9fdb0a894b8b873
Cornelia Kelinske <corneliakelinske@gmail.com>
2022-05-18 14:16:54 -0700

    get started on authentication

--- lib/pento_web/live/wrong_live.ex
+++ lib/pento_web/live/wrong_live.ex
@@ -1,12 +1,17 @@
 defmodule PentoWeb.WrongLive do
   use Phoenix.LiveView, layout: {PentoWeb.LayoutView, "live.html"}
-
   alias PentoWeb.Router.Helpers, as: Routes
 
-
-
-  def mount(_params, _session, socket) do
-    {:ok, assign(socket, score: 10, message: "Make a guess:", lucky_number: lucky_number(), time: time(), game_won?: false)}
+  def mount(_params, session, socket) do
+    {:ok,
+     assign(socket,
+       score: 10,
+       message: "Make a guess:",
+       lucky_number: lucky_number(),
+       time: time(),
+       game_won?: false,
+       session_id: session["live_socket_id"]
+     )}
   end
 
   def render(assigns) do
@@ -22,54 +27,61 @@ defmodule PentoWeb.WrongLive do
     <% end %>
     </h2>
     <%= if @game_won? == true do %>
-    <btn> <%= live_patch "Play again!", to: Routes.live_path(@socket, PentoWeb.WrongLive) %> </btn>
+    <btn> <%= live_redirect "Play again!", to: Routes.live_path(@socket, PentoWeb.WrongLive) %> </btn>
 
     <% end %>
+    <pre>
+    <%= @current_user.email %>
+    <%= @session_id %>
+    </pre>
 
     """
   end
 
-
-
-  def handle_event("guess", %{"number" => guess}=data, %{assigns: %{lucky_number: lucky_number}} = socket) do
+  def handle_event(
+        "guess",
+        %{"number" => guess} = data,
+        %{assigns: %{lucky_number: lucky_number}} = socket
+      ) do
     case guess do
-     ^lucky_number -> message = "You win"
-      {
-        :noreply,
-        assign(
-          socket,
-          message: message,
-          time: time(),
-          game_won?: true
-        )
-      }
-    _ ->
-    message = "Your guess: #{guess}. Wrong. Guess again. "
-    score = socket.assigns.score - 1
+      ^lucky_number ->
+        message = "You win"
 
+        {
+          :noreply,
+          assign(
+            socket,
+            message: message,
+            time: time(),
+            game_won?: true
+          )
+        }
 
+      _ ->
+        message = "Your guess: #{guess}. Wrong. Guess again. "
+        score = socket.assigns.score - 1
 
-    {
-      :noreply,
-      assign(
-        socket,
-        message: message,
-        score: score,
-        time: time()
-      )
-      }
+        {
+          :noreply,
+          assign(
+            socket,
+            message: message,
+            score: score,
+            time: time()
+          )
+        }
     end
   end
 
   def handle_params(_params, _url, socket) do
     {:noreply, socket}
-    end
+  end
 
   def lucky_number() do
     Enum.random(1..10) |> to_string
   end
 
   def time() do
-    DateTime.utc_now |> to_string
+    DateTime.utc_now() |> to_string
   end
 end
