041a0feabe6032eb708b007d9ddc84455352c4c2
Cornelia Kelinske <corneliakelinske@gmail.com>
2022-06-20 16:11:17 -0700

    complete function component chapter

--- lib/pento_web/live/demographic_live/show.ex
+++ lib/pento_web/live/demographic_live/show.ex
@@ -4,6 +4,13 @@ defmodule PentoWeb.DemographicLive.Show do
 
   def details(assigns) do
     ~H"""
+    <div class="survey-component-container">
+    <h2>Demographics <%= raw "&#x2713;" %></h2>
+    <ul>
+    <li>Gender: <%= @demographic.gender %></li>
+    <li>Year of birth: <%= @demographic.year_of_birth %></li>
+    </ul>
+    </div>
     """
   end
 end
--- lib/pento_web/live/survey_live.ex
+++ lib/pento_web/live/survey_live.ex
@@ -1,8 +1,18 @@
 defmodule PentoWeb.SurveyLive do
   use PentoWeb, :live_view
   alias __MODULE__.Component
+  alias PentoWeb.DemographicLive
+  alias Pento.Survey
 
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    {:ok,
+    socket
+    |> assign_demographic}
+  end
+
+  defp assign_demographic(%{assigns: %{current_user: current_user}} = socket) do
+    assign(socket,
+    :demographic,
+    Pento.Survey.get_demographic_by_user(current_user))
   end
 end
