1b88184624d4ce21597f59c6dd6a0fad6d95a6b2
Cornelia Kelinske <corneliakelinske@gmail.com>
2022-06-21 11:56:09 -0700

    get started with stateful compnonent - demographics -

--- lib/pento_web/live/survey_live.ex
+++ lib/pento_web/live/survey_live.ex
@@ -1,13 +1,24 @@
 defmodule PentoWeb.SurveyLive do
   use PentoWeb, :live_view
   alias __MODULE__.Component
-  alias PentoWeb.DemographicLive
+  alias PentoWeb.{DemographicLive, DemographicLive.Form}
   alias Pento.Survey
+
 
   def mount(_params, _session, socket) do
     {:ok,
     socket
     |> assign_demographic}
+  end
+
+  def handle_info({:created_demographic, demographic}, socket) do
+    {:noreply, handle_demographic_created(socket, demographic)}
+  end
+
+  def handle_demographic_created(socket, demographic) do
+    socket
+    |> put_flash(:info, "Demographic created successfully")
+    |> assign(:demographic, demographic)
   end
 
   defp assign_demographic(%{assigns: %{current_user: current_user}} = socket) do
