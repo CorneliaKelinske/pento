27351442b005a9801a9213989170062def8cf967
Cornelia Kelinske <corneliakelinske@gmail.com>
2022-06-23 07:59:50 -0700

    start to add ratings live component to survey liveview

--- lib/pento_web/live/survey_live.ex
+++ lib/pento_web/live/survey_live.ex
@@ -2,13 +2,14 @@ defmodule PentoWeb.SurveyLive do
   use PentoWeb, :live_view
   alias __MODULE__.Component
   alias PentoWeb.{DemographicLive, DemographicLive.Form}
-  alias Pento.Survey
+  alias Pento.{Survey, Catalog}
 
 
   def mount(_params, _session, socket) do
     {:ok,
     socket
-    |> assign_demographic}
+    |> assign_demographic()
+    |> assign_products()}
   end
 
   def handle_info({:created_demographic, demographic}, socket) do
@@ -25,5 +26,13 @@ defmodule PentoWeb.SurveyLive do
     assign(socket,
     :demographic,
     Pento.Survey.get_demographic_by_user(current_user))
+  end
+
+  defp assign_products(%{assigns: %{current_user: current_user}} = socket) do
+    assign(socket, :products, list_products(current_user))
+  end
+
+  defp list_products(user) do
+    Catalog.list_products_with_user_rating(user)
   end
 end
